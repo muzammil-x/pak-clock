6be1746dbd5e890ffa51a4a5a7d0cc7da22d4b11
muzammil-x <m.muzammilx2@gmail.com>
2024-07-15 10:11:06 +0500

    clock2.0

--- src/assets/components/Description.jsx
+++ src/assets/components/Description.jsx
@@ -1,4 +1,4 @@
 function Description() {
-  return <p>This is the App that show the date and time in Pakistan.</p>;
+  return <h5>This is the App that show the date and time in Pakistan.</h5>;
 }
 export default Description;
