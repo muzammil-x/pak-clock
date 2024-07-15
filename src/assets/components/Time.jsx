cf08fbf4ac0d0189d6351483f9f43f939568b8fd
muzammil-x <m.muzammilx2@gmail.com>
2024-07-15 10:14:28 +0500

    console.log removed

--- src/assets/components/Time.jsx
+++ src/assets/components/Time.jsx
@@ -4,14 +4,12 @@ function Time() {
   const [time, setTime] = useState(new Date());
 
   useEffect(() => {
-    console.log("interval setup");
     const intervalId = setInterval(() => {
       setTime(new Date());
     }, 1000);
 
     return () => {
       clearInterval(intervalId);
-      console.log("interval canceled");
     };
   }, []);
   return (
