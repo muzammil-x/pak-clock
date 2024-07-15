9e1d2517ac0d487728d01f5be1a0749a77442394
muzammil-x <m.muzammilx2@gmail.com>
2024-07-15 10:11:40 +0500

    update

--- src/assets/components/Time.jsx
+++ src/assets/components/Time.jsx
@@ -1,6 +1,19 @@
-function Time() {
-  let time = new Date();
+import { useEffect, useState } from "react";
 
+function Time() {
+  const [time, setTime] = useState(new Date());
+
+  useEffect(() => {
+    console.log("interval setup");
+    const intervalId = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+
+    return () => {
+      clearInterval(intervalId);
+      console.log("interval canceled");
+    };
+  }, []);
   return (
     <h3>
       This is the current time: {time.toLocaleDateString()} -
